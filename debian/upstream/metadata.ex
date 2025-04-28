b98ca608c24b0976111d8911079e5e667fc1f9a6
me-joe <ctf.beginer.joe@gmail.com>
2025-04-28 12:56:06 -0400

    Initial commit

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/ruby-winrm-fs/issues
+# Bug-Submit: https://github.com/<user>/ruby-winrm-fs/issues/new
+# Changelog: https://github.com/<user>/ruby-winrm-fs/blob/master/CHANGES
+# Documentation: https://github.com/<user>/ruby-winrm-fs/wiki
+# Repository-Browse: https://github.com/<user>/ruby-winrm-fs
+# Repository: https://github.com/<user>/ruby-winrm-fs.git
